cae42db82ff72c328b7d0bf70adfbcd1d2812e9e
Ryohei Suzuki <quolc.i@gmail.com>
2017-05-14 01:34:11 +0900

    add theme pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-  $('.bxslider').bxSlider();
+  if ($('.index-container').length == 1) {
+    $('.bxslider').bxSlider();
+  }
+  if ($('.exhibition-container').length == 1) {
+    $('.group').each(function() {
+      var h = $(this).find('.group-body').height();
+      if ($(this).height() < h) {
+        $(this).css('height', h);
+      }
+    });
+  }
 });
